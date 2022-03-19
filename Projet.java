6950fd5324152b36ce5ad66541edd57b550b6ef5
Guillaume Chevallier <51002806+GuitouNeoz@users.noreply.github.com>
2022-03-20 00:02:58 +0100

    message crypté

--- Projet.java
+++ Projet.java
@@ -1,4 +1,5 @@
 import java.io.UnsupportedEncodingException;
+import java.util.*;
 
 public class Projet {
 
@@ -11,20 +12,23 @@ public class Projet {
         }
     }
 
-    public static void xor(byte[] message,byte[] masque){
+    public static byte[] xor(byte[] message,byte[] masque){
 
-        int tailleM = message.length;
-
-        System.out.println(tailleM);
+        String messageBinaire = afficheEnBinaire(message);
+        int tailleM = messageBinaire.length();
+        String masqueBinaire = afficheEnBinaire(masque);
+        int tailleMasque = masqueBinaire.length();
 
         byte[] res = new byte[tailleM];
 
-        
+        for(int i=0;i<tailleM;i++){
+            res[i]=xor(message[i],masque[i]);
+        }
 
+        return res;
     }
 
     public static String afficheEnBinaire(byte[] message){
-
         String byteMessage = "";
 
         for(byte c : message){
@@ -35,7 +39,6 @@ public class Projet {
     }
 
     public static String afficheEnString(byte[] message){
-
         String stringMessage = "";
 
         for(byte c : message){
@@ -45,60 +48,59 @@ public class Projet {
         return stringMessage;
     }
 
-    public static byte[] genererMasque(){
+    public static byte[] genererMasque(byte[] message){
+        String messageBinaire = afficheEnBinaire(message);
+        int tailleM = messageBinaire.length();
+
+        byte[] masque = new byte[tailleM];
 
         //générer un masque aléatoire de longueur 16
-        byte[] masque = new byte[16];
-
         for(int i=0;i<16;i++){
-
             int b=(int)(Math.random()*2);  //nb aléatoire 0 ou 1
-
-            switch(b){
-                case 0 :{
-                    masque[i]=0;
-                    break;
-                }
-                case 1 :{
-                    masque[i]=1;
-                    break;
-                }
-            }
+            masque[i]=(byte) b;
         }
 
+        //on applique la formule du LFSR
+        for(int y=16;y<tailleM;y++){
+            masque[y] = xor(xor(xor(masque[y-5],masque[y-6]),masque[y-15]),masque[y-16]);
+        }
+        
         return masque;
     }
 
 
     
     public static void main (String[] args) throws UnsupportedEncodingException {
-
-        String message = "Je m'appelle Guillaume";
-        byte[] infoBin = message.getBytes("UTF-8");
-
         //affichage binaire de chacun des caractères du message
-        /*for (byte b : infoBin) {
+        
+        String m = "Je m'appelle Guillaume";
+        byte[] infoBin = m.getBytes("UTF-8");
+
+        for (byte b : infoBin) {
             System.out.println((char) b + "-> "
                     + Integer.toBinaryString(b));
-        }*/
+        }
 
-        byte a = 1;
-        byte b = 1;
+        //on converti en string puis en tableau de byte pour obtenir un message binaire
+        String messageString = afficheEnBinaire(infoBin);
+        byte[] message = new byte[messageString.length()];
 
-        String byteMessage = afficheEnBinaire(infoBin);
-        String stringMessage = afficheEnString(infoBin);
+        for(int y=0; y < messageString.length(); y++){
+            message[y] =(byte) Integer.parseInt(String.valueOf(messageString.charAt(y)));
+        }
 
-        //System.out.println(byteMessage);
-        //System.out.println(stringMessage);
+        System.out.println("message : "+afficheEnBinaire(message));
 
-        byte[] testMasque = null;
-        testMasque = genererMasque();
-        System.out.println(afficheEnBinaire(testMasque));
+        byte[] masque = null;
+        masque = genererMasque(infoBin);
 
-        xor(infoBin,testMasque);
+        System.out.println("message : "+afficheEnBinaire(message));
+        System.out.println("masque  : "+afficheEnBinaire(masque));
 
-        //byte res = xor(a,b);
-        //byte res2 = xor(a,b);
-        //ystem.out.println(res+" "+res2);
+        //message crypté en binaire
+        byte[] messageCrypte = null;
+        messageCrypte = xor(message,masque);
+        System.out.println("crypte  : "+afficheEnBinaire(messageCrypte));
+
     }
 }
